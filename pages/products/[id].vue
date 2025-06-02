73c086b0e8adb25adf5e15f94b7bed79777c2b4e
lojayrivz@gmail.com <louie@bposeats.com>
2025-06-03 02:08:16 +0800

    Add product route and page

---
+++ pages/products/[id].vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, quam dolore delectus quo excepturi impedit explicabo itaque accusamus illum ipsam?</p>
+    </div>
+</template>
+
+<script setup>
+    // export default {
+        
+    // }
+    const { id } = useRoute().params
+
+    definePageMeta({
+        layout: 'products'
+    })
+</script>
+
+<style scoped>
+
+</style>
