ffdf8bdd7cfd1cec70269fba9434e66b40382ab5
lojayrivz@gmail.com <louie@bposeats.com>
2025-06-03 00:08:38 +0800

    create about and index page

---
+++ pages/about.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque maiores rem voluptatem aliquam totam quidem, omnis veniam nam aliquid ipsum.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque maiores rem voluptatem aliquam totam quidem, omnis veniam nam aliquid ipsum.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        setup () {
+            
+
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+
+    p {
+        margin: 20px 0;
+    }
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>Products</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        setup () {
+            
+
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
