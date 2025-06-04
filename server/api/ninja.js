48fc5e3cea3bd5a2322372c55b0a810f2044a3e8
lojayrivz@gmail.com <louie@bposeats.com>
2025-06-05 02:06:28 +0800

    setup Dynamic server route

--- server/api/ninja.js
+++ server/api/ninja.js
@@ -7,11 +7,6 @@ export default defineEventHandler(async (event) => {
 
     const { age } = await readBody(event)
 
-    // api call with private key
-    // const { data } = await $fetch('https://api.currencyapi.com/v3/latest?apikey=eJdPhICJUDBt6B4e4IX6DZ4A9Xos4Z2JRZ8xh2v2')
-
-    // return data
-
     return {
         message: `Hello, ${name}! You are ${age} years old.`
     }
