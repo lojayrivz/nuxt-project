1d415c14c9c23f1619a6c158e7260598847eb995
lojayrivz@gmail.com <louie@bposeats.com>
2025-06-05 01:38:24 +0800

    Configure Metadata and useHead

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,4 +16,16 @@ export default defineNuxtConfig({
   ],
 
   css: ['@/assets/css/tailwind.css'],
+
+  app: {
+    head: {
+      title: 'Nuxt Dojo',
+      meta: [
+        { name: 'description', content: 'Everything about Nuxt 3'}
+      ],
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
+      ]
+    }
+  }
 })
